6ee13387aff296ec1fcfd912b7e1832b219caa5c
rickkky <rickkky@foxmail.com>
2023-06-02 23:10:35 +0800

    test: update vector3 tests; refactor vector2 tests

--- test/vector2.test.ts
+++ test/vector2.test.ts
@@ -1,107 +1,103 @@
 import { describe, expect, test } from 'vitest';
-import { vec2 } from '/src/vector2';
+import { Vector2, vec2 } from '/src/vector2';
 
 describe('vector2', () => {
+  test('constructor', () => {
+    const v0 = new Vector2(1, 2);
+    expect(v0.dimension).toBe(2);
+    expect(v0[0]).toBe(1);
+    expect(v0.x).toBe(1);
+    expect(v0[1]).toBe(2);
+    expect(v0.y).toBe(2);
+    expect(v0.size).toBe(Math.hypot(1, 2));
+  });
+
   test('create', () => {
-    const v = vec2(1, 2);
-    expect(v[0]).toBe(1);
-    expect(v.x).toBe(1);
-    expect(v[1]).toBe(2);
-    expect(v.y).toBe(2);
-    expect(v.dimension).toBe(2);
-    expect(v.size).toBe(Math.hypot(1, 2));
+    const v0 = vec2(1, 2);
+    expect(v0.dimension).toBe(2);
+    expect(v0[0]).toBe(1);
+    expect(v0.x).toBe(1);
+    expect(v0[1]).toBe(2);
+    expect(v0.y).toBe(2);
+    expect(v0.size).toBe(Math.hypot(1, 2));
+  });
+
+  test('equal', () => {
+    expect(vec2(1, 2).equal(vec2(1, 2))).toBe(true);
+    expect(vec2(1, 2).equal(vec2(2, 1))).toBe(false);
+  });
+
+  test('constructor overloads', () => {
+    const v0 = new Vector2(1, 2);
+    const v1 = new Vector2([1, 2]);
+    expect(v1.equal(v0)).toBe(true);
+    const v2 = new Vector2(v0);
+    expect(v2.equal(v0)).toBe(true);
+  });
+
+  test('create overloads', () => {
+    const v0 = vec2(1, 2);
+    const v1 = vec2([1, 2]);
+    expect(v1.equal(v0)).toBe(true);
+    const v2 = vec2(v0);
+    expect(v2.equal(v0)).toBe(true);
   });
 
   test('setter', () => {
     const v = vec2(1, 2);
     v.set(3, 4);
-    expect(v[0]).toBe(3);
-    expect(v.x).toBe(3);
-    expect(v[1]).toBe(4);
-    expect(v.y).toBe(4);
+    expect(v.equal(vec2(3, 4))).toBe(true);
     v[0] = 5;
     v[1] = 6;
-    expect(v[0]).toBe(5);
-    expect(v.x).toBe(5);
-    expect(v[1]).toBe(6);
-    expect(v.y).toBe(6);
+    expect(v.equal(vec2(5, 6))).toBe(true);
     v.x = 7;
     v.y = 8;
-    expect(v[0]).toBe(7);
-    expect(v.x).toBe(7);
-    expect(v[1]).toBe(8);
-    expect(v.y).toBe(8);
+    expect(v.equal(vec2(7, 8))).toBe(true);
   });
 
   test('2d getter', () => {
     const v = vec2(1, 2);
-    const xy = v.xy;
-    expect(xy[0]).toBe(1);
-    expect(xy.x).toBe(1);
-    const yx = v.yx;
-    expect(yx[0]).toBe(2);
-    expect(yx.x).toBe(2);
+    expect(v.xy.equal(vec2(1, 2)));
+    expect(v.yx.equal(vec2(2, 1)));
   });
 
   test('clone', () => {
     const v = vec2(1, 2);
     const c = v.clone();
-    expect(c[0]).toBe(1);
-    expect(c.x).toBe(1);
-    expect(c[1]).toBe(2);
-    expect(c.y).toBe(2);
-    expect(c.dimension).toBe(2);
-    expect(c.size).toBe(Math.hypot(1, 2));
+    expect(c).not.toBe(v);
+    expect(c.equal(v)).toBe(true);
   });
 
   test('iterator', () => {
     const v = vec2(1, 2);
     const array = [...v];
-    expect(array[0]).toBe(1);
-    expect(array[1]).toBe(2);
-  });
-
-  test('equal', () => {
-    const v1 = vec2(1, 2);
-    const v2 = vec2(1, 2);
-    const v3 = vec2(2, 1);
-    expect(v1.equal(v2)).toBe(true);
-    expect(v1.equal(v3)).toBe(false);
+    expect(array).toEqual([1, 2]);
   });
 
   test('add', () => {
-    const v1 = vec2(1, 2);
-    const v2 = vec2(2, 1);
-    v1.add(v2);
-    expect(v1.x).toBe(3);
-    expect(v1.y).toBe(3);
+    const v = vec2(1, 2).add(vec2(2, 1));
+    expect(v.equal(vec2(3, 3))).toBe(true);
   });
 
   test('substract', () => {
-    const v1 = vec2(1, 2);
-    const v2 = vec2(2, 1);
-    v1.substract(v2);
-    expect(v1.x).toBe(-1);
-    expect(v1.y).toBe(1);
+    const v = vec2(1, 2).substract(vec2(2, 1));
+    expect(v.equal(vec2(-1, 1))).toBe(true);
   });
 
   test('scale', () => {
-    const v1 = vec2(1, 2);
-    v1.scale(2);
-    expect(v1.x).toBe(2);
-    expect(v1.y).toBe(4);
+    const v = vec2(1, 2).scale(2);
+    expect(v.equal(vec2(2, 4))).toBe(true);
   });
 
   test('normalize', () => {
-    const v1 = vec2(1, 2);
-    const v2 = v1.clone().normalize();
-    expect(v2.x).toBe(v1.x / v1.size);
-    expect(v2.y).toBe(v1.y / v1.size);
+    const v0 = vec2(1, 2);
+    const v1 = v0.clone().normalize();
+    expect(v1.equal(v0.clone().scale(1 / v0.size))).toBe(true);
   });
 
   test('dot', () => {
-    const v1 = vec2(1, 2);
-    const v2 = vec2(2, 1);
-    expect(v1.dot(v2)).toBe(4);
+    const v0 = vec2(1, 2);
+    const v1 = vec2(2, 1);
+    expect(v0.dot(v1)).toBe(4);
   });
 });
---
+++ test/vector3.test.ts
@@ -0,0 +1,119 @@
+import { describe, expect, test } from 'vitest';
+import { Vector3, vec3 } from '/src/vector3';
+
+describe('vector3', () => {
+  test('constructor', () => {
+    const v0 = new Vector3(1, 2, 3);
+    expect(v0.dimension).toBe(3);
+    expect(v0[0]).toBe(1);
+    expect(v0.x).toBe(1);
+    expect(v0[1]).toBe(2);
+    expect(v0.y).toBe(2);
+    expect(v0[2]).toBe(3);
+    expect(v0.size).toBe(Math.hypot(1, 2, 3));
+  });
+
+  test('create', () => {
+    const v0 = vec3(1, 2, 3);
+    expect(v0.dimension).toBe(3);
+    expect(v0[0]).toBe(1);
+    expect(v0.x).toBe(1);
+    expect(v0[1]).toBe(2);
+    expect(v0.y).toBe(2);
+    expect(v0[2]).toBe(3);
+    expect(v0.size).toBe(Math.hypot(1, 2, 3));
+  });
+
+  test('equal', () => {
+    expect(vec3(1, 2, 3).equal(vec3(1, 2, 3))).toBe(true);
+    expect(vec3(1, 2, 3).equal(vec3(3, 2, 1))).toBe(false);
+  });
+
+  test('constructor overloads', () => {
+    const v0 = new Vector3(1, 2, 3);
+    const v1 = new Vector3([1, 2, 3]);
+    expect(v1.equal(v0)).toBe(true);
+    const v2 = new Vector3(v0);
+    expect(v2.equal(v0)).toBe(true);
+    const v3 = new Vector3(v0.xy, 3);
+    expect(v3.equal(v0)).toBe(true);
+    const v4 = new Vector3(1, v0.yz);
+    expect(v4.equal(v0)).toBe(true);
+  });
+
+  test('create overloads', () => {
+    const v0 = vec3(1, 2, 3);
+    const v1 = vec3([1, 2, 3]);
+    expect(v1.equal(v0)).toBe(true);
+    const v2 = vec3(v0);
+    expect(v2.equal(v0)).toBe(true);
+    const v3 = vec3(v0.xy, 3);
+    expect(v3.equal(v0)).toBe(true);
+    const v4 = vec3(1, v0.yz);
+    expect(v4.equal(v0)).toBe(true);
+  });
+
+  test('setter', () => {
+    const v = vec3(1, 2, 3);
+    v.set(4, 5, 6);
+    expect(v.equal(vec3(4, 5, 6))).toBe(true);
+    v[0] = 7;
+    v[1] = 8;
+    v[2] = 9;
+    expect(v.equal(vec3(7, 8, 9))).toBe(true);
+    v.x = 10;
+    v.y = 11;
+    v.z = 12;
+    expect(v.equal(vec3(10, 11, 12))).toBe(true);
+  });
+
+  test('2d getter', () => {
+    const v = vec3(1, 2, 3);
+    expect(v.xy.equal(vec3(1, 2)));
+    expect(v.xz.equal(vec3(1, 3)));
+    expect(v.yx.equal(vec3(2, 1)));
+    expect(v.yz.equal(vec3(2, 3)));
+    expect(v.zx.equal(vec3(3, 1)));
+    expect(v.zy.equal(vec3(3, 2)));
+  });
+
+  test('clone', () => {
+    const v = vec3(1, 2, 3);
+    const c = v.clone();
+    expect(c).not.toBe(v);
+    expect(c.equal(v)).toBe(true);
+  });
+
+  test('iterator', () => {
+    const v = vec3(1, 2, 3);
+    const array = [...v];
+    expect(array).toEqual([1, 2, 3]);
+  });
+
+  test('add', () => {
+    const v = vec3(1, 2, 3).add(vec3(3, 2, 1));
+    expect(v.equal(vec3(4, 4, 4))).toBe(true);
+  });
+
+  test('substract', () => {
+    const v = vec3(1, 2, 3).substract(vec3(3, 2, 1));
+    expect(v.equal(vec3(-2, 0, 2))).toBe(true);
+  });
+
+  test('scale', () => {
+    const v = vec3(1, 2, 3).scale(2);
+    expect(v.equal(vec3(2, 4, 6))).toBe(true);
+  });
+
+  test('normalize', () => {
+    const v0 = vec3(1, 2, 3);
+    const v1 = v0.clone().normalize();
+    expect(v1.equal(v0.clone().scale(1 / v0.size))).toBe(true);
+  });
+
+  test('dot', () => {
+    const v0 = vec3(1, 2, 3);
+    const v1 = vec3(3, 2, 1);
+    expect(v0.dot(v1)).toBe(10);
+  });
+});
