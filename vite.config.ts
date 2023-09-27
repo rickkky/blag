bf0f8e946e9ce3afb10d9a079e56bd3386967a98
rickkky <rickkky@foxmail.com>
2023-09-27 22:36:35 +0800

    build: update output lib config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig(() => {
     build: {
       lib: {
         entry: resolve(__dirname, 'src/index.ts'),
-        name: 'VMATRIX',
-        fileName: 'v-matrix',
+        name: 'g',
+        fileName: 'g-matrix',
       },
     },
     server: {
